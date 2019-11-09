b1df2032760c84ac465542b305a561044422ca69
Michael Hung <myshung@ucalgary.ca>
2019-11-09 11:51:38 -0700

    tweak friction and bounciness values, added coefficient and linear easing function, added speed threshold to despawn ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public Vector3 InitialVelocity;
+    public float SpeedCoefficient;
+    public float DespawnThreshold;
 
     private Rigidbody rb;
     private SphereCollider sc;
@@ -12,8 +14,11 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        SpeedCoefficient = 1;
+
         rb = this.gameObject.GetComponent<Rigidbody>();
         rb.AddForce(InitialVelocity * 2, ForceMode.Impulse);
+
         sc = this.gameObject.GetComponent<SphereCollider>();
         
     }
@@ -23,19 +28,19 @@ public class Ball : MonoBehaviour
 
         if (collision.gameObject.tag.Contains("Obstacle_Flat"))
         {
-            rb.AddForce(rb.velocity, ForceMode.Impulse);
+            rb.AddForce(rb.velocity * SpeedCoefficient, ForceMode.Impulse);
+            SpeedCoefficient *= 0.9f; // Easing function - how will the coefficient decrease over time
         }
-    }
-
-    private void FixedUpdate()
-    {
-        
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(rb.velocity.magnitude);
+
+        if (rb.velocity.magnitude <= DespawnThreshold) // Stop Ball if velocity is lower than Threshold.
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
