ae951c9300fe7418f27c07f67a14844e52c5c8df
Alan <saeroc@outlook.com>
2019-11-09 21:04:26 -0700

    Level Select Complete (No Flashing)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,7 +5,29 @@ public class Menu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+
+
+    public void EndGame()
+    {
+        Application.Quit();
     }
 
 }
