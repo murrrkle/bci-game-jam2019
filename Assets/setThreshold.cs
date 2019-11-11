7270c8318ea43d8a797f0b0bf3b2b673a8166db3
Alan <saeroc@outlook.com>
2019-11-10 20:16:39 -0700

    Colour Bars with Threshold

---
+++ Assets/setThreshold.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setThreshold : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float threshold;
+    private float thresholdAdjusted;
+
+    private float thresholdHeight;
+
+    private float barheight;
+    void Start()
+    {
+        barheight = 50;
+        thresholdHeight = 10;
+        thresholdAdjusted = threshold*barheight + thresholdHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(threshold > 0)
+        {
+            SetTransformX(thresholdAdjusted);
+        }
+        
+    }
+
+    void SetTransformX(float n)
+    {
+        transform.position = new Vector3(transform.position.x, n, transform.position.z);
+    }
+}
